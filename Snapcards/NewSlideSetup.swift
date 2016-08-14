95c3b21fa287d5e409f02e4825391d3879875da9
Mark Pinto <markpinto@Marks-MacBook-Pro.local>
2016-08-13 23:16:12 -0500

    Update

---
+++ Snapcards/NewSlideSetup.swift
@@ -0,0 +1,9 @@
+//
+//  NewSlideSetup.swift
+//  Snapcards
+//
+//  Created by FHS MM2 on 3/1/16.
+//  Copyright © 2016 EforEpsilon. All rights reserved.
+//
+
+import Foundation
--- Snapcards/ViewController.swift
+++ Snapcards/ViewController.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+func getDocumentsURL() -> NSURL {
+    let documentsURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+    return documentsURL
+}
+
+func fileInDocumentsDirectory(filename: String) -> String {
+    
+    let fileURL = getDocumentsURL().URLByAppendingPathComponent(filename)
+    return fileURL.path!
+    
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -21,5 +33,124 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+
+class NewSlideSetup: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet weak var currentImage: UIImageView!
+    @IBOutlet weak var dekTitle: UINavigationItem!
+    
+    let imagePicker: UIImagePickerController! = UIImagePickerController()
+    let myImageName = "image.png"
+    //let imagePath = fileInDocumentsDirectory(myImageName)
+    let titleAlert = UIAlertController(title: "New Deck Created", message: "Please name the deck.", preferredStyle: .Alert)
+    //var deckTitle: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.scrollViewWidth
+        imagePicker.delegate = self
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        titleAlert.addTextFieldWithConfigurationHandler({ (textField) -> Void in
+            textField.text = "Some default text."
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+            // ...
+        }
+        titleAlert.addAction(cancelAction)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            self.dekTitle.title = self.titleAlert.textFields![0].text
+            
+        }
+        titleAlert.addAction(OKAction)
+        
+        self.presentViewController(titleAlert, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getDocumentsDirectory() -> NSString {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    @IBAction func takePicture(sender: UIBarButtonItem) {
+        if (UIImagePickerController.isSourceTypeAvailable(.Camera)) {
+            if UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil {
+                imagePicker.allowsEditing = false
+                imagePicker.sourceType = .Camera
+                imagePicker.cameraCaptureMode = .Photo
+                presentViewController(imagePicker, animated: true, completion: {})
+            } else {
+                postAlert("Rear camera doesn't exist", message: "Application cannot access the camera.")
+            }
+        } else {
+            postAlert("Camera inaccessable", message: "Application cannot access the camera.")
+        }
+    }
+    
+    @IBAction func choosePicture(sender: UIBarButtonItem) {
+        if (UIImagePickerController.isSourceTypeAvailable(.Camera)) {
+            if UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil {
+                imagePicker.allowsEditing = false
+                imagePicker.sourceType = .PhotoLibrary
+                presentViewController(imagePicker, animated: true, completion: {})
+            } else {
+                postAlert("Rear camera doesn't exist", message: "Application cannot access the camera.")
+            }
+        } else {
+            postAlert("Camera inaccessable", message: "Application cannot access the camera.")
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print("Got an image")
+        if let pickedImage:UIImage = (info[UIImagePickerControllerOriginalImage]) as? UIImage {
+            let selectorToCall = Selector("imageWasSavedSuccessfully:didFinishSavingWithError:context:")
+            UIImageWriteToSavedPhotosAlbum(pickedImage, self, selectorToCall, nil)
+        }
+        imagePicker.dismissViewControllerAnimated(true, completion: {
+            // Anything you want to happen when the user saves an image
+        })
+    }
+    
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        print("User canceled image")
+        dismissViewControllerAnimated(true, completion: {
+            // Anything you want to happen when the user selects cancel
+        })
+    }
+    
+    func imageWasSavedSuccessfully(image: UIImage, didFinishSavingWithError error: NSError!, context: UnsafeMutablePointer<()>){
+        print("Image saved")
+        if let theError = error {
+            print("An error happened while saving the image = \(theError)")
+        } else {
+            print("Displaying")
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.currentImage.image = image
+            })
+        }
+    }
+    
+    func postAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message,
+            preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
 }
 
